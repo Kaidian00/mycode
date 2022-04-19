748e4a4079cd281643a8cdb7597f3b36d2d70113
Kaidian <kaidian00@users.noreply.github.com>
2022-04-19 21:09:55 +0000

    While looping

---
+++ custif/travel.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   if, elif, else - A simple program using conditionals to make a decision."""
+
+
+message = 'Platinum Memeber.'
+
+# wrap destination in a float() to accept decimals as
+destination = float(input("What is your next Caribbean Destination?" Baha))
+
+rps = {"Bahamas":1, "Cuba":2, "Anguilla":3, "Tortola",4}
+
+# if input value was higher or equal to
+if destination == "Bahamas":
+    print('Welcome!')
+
+print(message)
+
